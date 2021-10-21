7b41352c594643ba2fefb6f10df1ce2a302056fb
Robert Tierie <70277787+roberttierie@users.noreply.github.com>
2021-10-21 19:35:08 +0100

    Add files via upload

---
+++ src/components/Results.js
@@ -0,0 +1,16 @@
+
+// import './App.css';
+// import React, {useState, useEffect} from 'react'
+// import * as yup from 'yup';
+// import axios from "axios";
+
+
+
+// const Results = () => {
+//     return(
+//         <div>
+
+//         </div>
+//     )
+// }
+// export default Results; 
